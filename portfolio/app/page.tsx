63559d10cfbe962ecb0837541b51460aec680d03
ramu siripalli <ramsiripalli2772@gmail.com>
2026-05-25 11:30:55 +0530

    Initial Next.js Portfolio setup- Ramu

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <Button>
+        My Portfolio
+      </Button>
+    </main>
+  );
+}
